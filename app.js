dc159c3fad3189cdd166607cce954ff5f5747047
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-12-27 19:09:57 +0100

    fix setTimeout

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const tabellone = document.getElementById('tabellone')
 const estrazione = document.getElementById("estrazione")
 const buttons = document.getElementById("game")
 
-let all_numbers = [];
+let all_numbers = []
 for (let i = 1; i <= 90; i++) {
-  all_numbers.push(i);
+  all_numbers.push(i)
 }
 console.log(all_numbers)
 
@@ -18,54 +18,46 @@ function createTable() {
   });
 
 }
-createTable();
+createTable()
 
 function estraction(event) {
   if (all_numbers.length === 0) {
     alert("Tutti i numeri sono stati estratti!\nClicca su termina gioco per una nuova partita")
-    return
+    return;
   }
 
+  const randomIndex = Math.floor(Math.random() * all_numbers.length);
+  const numeroCasuale = all_numbers[randomIndex]
+  console.log("Numero estratto: " + numeroCasuale)
+
+  // Rimuove il numero estratto dall'array
+  all_numbers.splice(randomIndex, 1)
+  console.log("Numeri rimanenti: " + all_numbers)
+
+  // Cerca un elemento esistente con la classe 'button-numCasual'
+  let numberSelected = document.querySelector('.button-numCasual')
+
+  if (!numberSelected) {
+    numberSelected = document.createElement('div')
+    numberSelected.className = 'button-numCasual'
+    buttons.appendChild(numberSelected)
+  }
+
+  // Aggiorna il contenuto con il nuovo numero estratto
+  numberSelected.textContent = '. . .'
+
   setTimeout(() => {
-    const randomIndex = Math.floor(Math.random() * all_numbers.length)
-    const numeroCasuale = all_numbers[randomIndex] // Estrae un numero casuale dall'array
-    console.log("Numero estratto: " + numeroCasuale)
+    numberSelected.textContent = numeroCasuale
 
-
-    // Rimuove il numero estratto dall'array
-    all_numbers.splice(randomIndex, 1)
-    console.log("Numeri rimanenti:" + all_numbers)
-
-    // Cerca un elemento esistente con la classe 'button-numCasual'
-    let numberSelected = document.querySelector('.button-numCasual')
-
-    if (!numberSelected) {
-      numberSelected = document.createElement('div')
-      numberSelected.className = 'button-numCasual'
-      buttons.appendChild(numberSelected);
-    }
-
-    // Aggiorna il contenuto con il nuovo numero estratto
-    setTimeout(() => {
-      numberSelected.textContent = numeroCasuale
-    }, 1000)
-
-    numberSelected.textContent = '. . .'
-
-    const buttonsInTabellone = document.querySelectorAll('.button-tabellone') //NodeList
-
-
-    //Trasformiamo la NodeList in un Array per utilizzare il metodo find
-    const buttonToActivate = Array.from(buttonsInTabellone).find(button => button.textContent == numeroCasuale)
+    const buttonToActivate = Array.from(document.querySelectorAll('.button-tabellone')) //NodeList
+      .find(button => button.textContent == numeroCasuale) //Trasformiamo la NodeList in un Array per utilizzare il metodo find
 
     if (buttonToActivate) {
-      setTimeout(() => {
-        buttonToActivate.classList.add('active');
-      }, 1000)
+      buttonToActivate.classList.add('active')
     }
-
-  }, 1000)
+  }, 1000);
 }
+
 
 function endGame() {
   alert("Tombolaaa")
